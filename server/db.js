ccbb33401de340965f5f7ffc691585b10234b2a4
Derek Tissaw <tissawd6@gmail.com>
2021-07-09 19:56:10 -0500

    initial db connection

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const { MongoClient } = require('mongodb');
+
+
+const dbUri = 'mongodb://127.0.0.1:27017';
+
+const client = new MongoClient(dbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+module.exports = client;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const client = require('./db');
 
 const app = express();
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello')
 });
 
-app.listen(8080, () => {
-  console.log("app listening on port 8080")
-});
+const run = async () => {
+  try { 
+    await client.connect();
+    console.log('db connected')
+  }
+    catch {
+      console.log('Something went wrong')
+    }
+
+
+    app.listen(8080, () => {
+      console.log("app listening on port 8080")
+    })
+  
+  
+  
+};
+
+run();
+
+
+
