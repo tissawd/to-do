265aa59f6b1f1c1ef42726baa8c587da71b0669b
Derek Tissaw <tissawd6@gmail.com>
2021-07-09 20:14:47 -0500

    db connection questionable

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 
-const dbUri = 'mongodb://127.0.0.1:27017';
+const dbUri = 'mongodb://127.0.0.1:27017/todo';
 
 const client = new MongoClient(dbUri, {
   useNewUrlParser: true,
--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const client = require('./db');
+const { client } = require('./db');
 
 const app = express();
 
 
 
 
-app.get('/', (req, res) => {
-  res.send('Hello')
+app.get('/', async (req, res) => {
+  const taskList = await client.listDatabases()
+  console.log(taskList);
+  res.send(taskList)
 });
 
 const run = async () => {
   try { 
     await client.connect();
     console.log('db connected')
+    // client.tasks.insertOne({"task": "task from the db", "complete": "false"})
   }
     catch {
       console.log('Something went wrong')
